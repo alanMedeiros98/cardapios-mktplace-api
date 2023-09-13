88567fbb1e657bfab6b376ebd151b9e8b68c8da4
Alan Duarte de Medeiros <alantuba2012@gmail.com>
2023-09-12 22:12:41 -0300

    Iniciado implementação do item 3.4 conforme especificação

--- src/main/java/br/com/senai/cardapiosmktplaceapi/dto/CardapioSalvo.java
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/dto/CardapioSalvo.java
@@ -1,18 +1,19 @@
 package br.com.senai.cardapiosmktplaceapi.dto;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import br.com.senai.cardapiosmktplaceapi.entity.Restaurante;
+import br.com.senai.cardapiosmktplaceapi.entity.enums.Status;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
-public class NovoCardapio {
+public class CardapioSalvo {
+
+	@NotNull(message = "O id é obrigatório")
+	@Positive(message = "O id do cardápio deve ser positivo")
+	private Integer id;
 	
 	@Size(max = 100, message = "O nome do cardápio não deve conter mais de 100 caracteres")
 	@NotBlank(message = "O nome é obrigatório")
@@ -23,12 +24,8 @@ public class NovoCardapio {
 	
 	@NotNull(message = "O restaurante é obrigatório")
 	private Restaurante restaurante;
-
-	@Size(min = 1, message = "O cardapio deve possuir ao menos 1 opção")
-	private List<NovaOpcaoDoCardapio> opcoes;
 	
-	public NovoCardapio() {
-		this.opcoes = new ArrayList<>();
-	}
+	@NotNull(message = "O status é obrigatório")
+	private Status status;
 	
 }
