e1017acd47e0776ff77552f061a1fe5af94dd298
Alan Duarte de Medeiros <alantuba2012@gmail.com>
2023-10-05 20:58:59 -0300

    Finalização da integração do envio de e-mails

--- src/main/java/br/com/senai/cardapiosmktplaceapi/service/proxy/RestauranteServiceProxy.java
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/service/proxy/RestauranteServiceProxy.java
@@ -1,11 +1,14 @@
 package br.com.senai.cardapiosmktplaceapi.service.proxy;
 
+import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import br.com.senai.cardapiosmktplaceapi.dto.Notificacao;
 import br.com.senai.cardapiosmktplaceapi.entity.Categoria;
 import br.com.senai.cardapiosmktplaceapi.entity.Restaurante;
 import br.com.senai.cardapiosmktplaceapi.entity.enums.Status;
@@ -18,11 +21,35 @@ public class RestauranteServiceProxy implements RestauranteService {
 	@Qualifier("restauranteServiceImpl")
 	private RestauranteService service;
 	
+	@Value("${email.endereco-admin}")
+	private String enderecoDeEmail;
+	
+	@Autowired
+	private ProducerTemplate toEmail;
+	
 	@Override
 	public Restaurante salvar(Restaurante resturante) {
-		return service.salvar(resturante);
+		Restaurante restauranteSalvo = service.salvar(resturante);
+		Notificacao notificacao = gerarNotificacaoPara(restauranteSalvo);
+		this.toEmail.sendBody("direct:enviarEmail", notificacao);
+		return restauranteSalvo;
 	}
 
+	private Notificacao gerarNotificacaoPara(Restaurante restaurante) {
+		Notificacao notificacao = new Notificacao();
+		notificacao.setDestinatario(enderecoDeEmail);
+		notificacao.setTitulo("Restaurante Criado/Atualizado");
+		StringBuilder texto = new StringBuilder();
+		texto.append("<p>Olá,</p>");
+		texto.append("<p>O restaurante <b>").append(restaurante.getNome() + "</b> foi criado ou atualizado no sistema</p>");
+		texto.append("<p>Esse email é automatico. Não deve ser respondido.</p>");
+		texto.append("<p>Atenciosamente,</p>");
+		texto.append("<p><b>Mktplace SENAI</b></p>");
+		notificacao.setMensagem(texto.toString());
+		
+		return notificacao;
+	}
+	
 	@Override
 	public void atualizarStatusPor(Integer id, Status status) {
 		this.service.atualizarStatusPor(id, status);
